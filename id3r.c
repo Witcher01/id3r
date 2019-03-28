ece3b2e467730a16fc3430fc5b760d1ce5b94185
Thomas <bthw@posteo.net>
2019-03-28 14:05:35 +0100

    added description and reordered functions

--- id3r.c
+++ id3r.c
@@ -1,8 +1,19 @@
+/* id3r is a small program to read only id3v1 tags. */
+/* neither the extended tag nor the id3v2 formats are supported */
+
 #include <stdlib.h>
 
 #include "id3v1.h"
 
 void usage(void);
+
+void
+usage(void)
+{
+    fputs("usage: id3r filename\n", stderr);
+
+    exit(EXIT_FAILURE);
+}
 
 int
 main(int argc, char *argv[])
@@ -21,12 +32,4 @@ main(int argc, char *argv[])
     }
 
     return EXIT_SUCCESS;
-}
-
-void
-usage(void)
-{
-    fputs("usage: id3r filename\n", stderr);
-
-    exit(EXIT_FAILURE);
 }
