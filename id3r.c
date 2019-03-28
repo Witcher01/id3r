1aa7eef17892069fab528c922d949c77c86f632b
Thomas <bthw@posteo.net>
2019-03-28 13:23:45 +0100

    added usage function

--- id3r.c
+++ id3r.c
@@ -2,19 +2,27 @@
 
 #include "id3v1.h"
 
+void usage(void);
+
 int
 main(int argc, char *argv[])
 {
     FILE *f;
 
     if (argc != 2) {
-        fputs("usage: id3r filename\n", stderr);
-
-        exit(EXIT_FAILURE);
+        usage();
     }
 
     f = fopen(argv[1], "rb");
     printid3v1(f);
 
     return EXIT_SUCCESS;
+}
+
+void
+usage(void)
+{
+    fputs("usage: id3r filename\n", stderr);
+
+    exit(EXIT_FAILURE);
 }
