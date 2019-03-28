319ae6d958ac336e6b614fa8b38fa3396d0382f8
Thomas <bthw@posteo.net>
2019-03-28 13:23:56 +0100

    empty buffer before writing
    empty the buffer the temporary music information is stored in to prevent
    corrupted text. this only has to be done when less bytes are read than
    before since otherwise these will only be overwritten.

--- id3v1.c
+++ id3v1.c
@@ -53,6 +53,9 @@ printid3v1(FILE *f)
     fread(buffer, 30, 1, f);
     printf("Album: %s\n", buffer);
 
+    /* empty the buffer before writing 4 bytes */
+    /* last byte can be set to 0 since 0 == '\0' */
+    memset(buffer, 0, sizeof(buffer));
     /* read 4 bytes for the year */
     fread(buffer, 4, 1, f);
     printf("Year: %s\n", buffer);
@@ -61,6 +64,8 @@ printid3v1(FILE *f)
     fread(buffer, 30, 1, f);
     printf("Comment: %s\n", buffer);
 
+    /* empty the buffer before writing 1 byte */
+    memset(buffer, 0, sizeof(buffer));
     /* read 1 byte for the genre */
     fread(buffer, 1, 1, f);
     printf("Genre: %d\n", buffer[0]);
